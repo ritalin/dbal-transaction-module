28304f8a7c06c20cf9cd42eee6ca2eb77721f555
Kazuhiko TAMURA <tamura@tecnet.co.jp>
2015-07-03 18:10:01 +0900

    first commit

---
+++ src/DbalTransaction.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace TransactionApi\Dbal;
+
+use Doctrine\DBAL\Driver\Connection;
+
+class DbalTransaction implements TransactionInterface
+{
+    /**
+     * @var Connection
+     */
+    private $conn;
+    
+    /**
+     * @param Connection conn
+     */
+    public function __constrruct(Connection $conn, Annotation\Transactional $annotation)
+    {
+        $this->conn = $conn;
+        $this->conn->setNestTransactionsWithSavepoints($annotation->txType === TransactionScope::REQUIRES_NEW);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function begin()
+    {
+        $this->conn->beginTransaction();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function commit()
+    {
+        $this->conn->commit();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function rollback()
+    {
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function inTransaction()
+    {
+        return $this->depth() > 0;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function depth()
+    {
+        return $this->conn->getTransactionNestingLevel();
+    }
+}
---
+++ src/TransactionScopeInterceptor.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TransactionApi\Dbal;
+
+use Ray\Aop\MethodInterceptor;
+use Ray\Aop\MethodInvocation;
+use Ray\Di\Exception\NotFound;
+
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\Common\Annotations\Reader;
+
+use TransactionApi\Annotation\Transactional;
+
+class TransactionScopeInterceptor implements MethodInterceptor
+{
+    /**
+     * @var Connection
+     */
+    private $conn;
+
+    /**
+     * @var Reader
+     */
+    private $reader;
+    
+    public function __construct(Connection $conn, Reader $reader)
+    {
+        $this->conn = $conn;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function invoke(MethodInvocation $invocation)
+    {
+        $annotation = $this->extractAnnotation($invocation->getMethod(), Transactional::class);
+        
+        $scope = new TransactionScope(new DbalTransaction($conn, $annotation), $annotation);
+        
+        return $scope->runInto(function() use ($invocation) {
+            return $invocation->proceed();
+        });
+    }
+    
+    private function extractAnnotation(\ReflectionMethod $m, $typeName)
+    {
+        if (($a = $this->reader->getMethodAnnotation($m, $typeName)) === null) {
+            throw new NotFound("Annotation: $typeName is not found.");
+        }
+        
+        return $a;
+    }
+}
